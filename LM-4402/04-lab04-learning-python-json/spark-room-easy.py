7d72e88125eef0a1055d4e5ff96a507d75db09f4
Divya18301 <divya.bhavani@huntington.com>
2017-05-15 15:22:16 -0400

    Update spark-room-easy.py

--- LM-4402/04-lab04-learning-python-json/spark-room-easy.py
+++ LM-4402/04-lab04-learning-python-json/spark-room-easy.py
@@ -6,7 +6,7 @@ accessToken = "" #put your access token between the quotes.
 
 accessToken_hdr = 'Bearer ' + accessToken
 spark_header = {'Authorization': accessToken_hdr}
-uri = 'https://api.ciscospark.com/v1/rooms'
+uri = 'MWU1ZDE4YzItMjllMi00YjdiLWExYzktZDNkODFlZDk4N2FjOTJmNjI4MTQtZWZl'
 resp = requests.get(uri, headers=spark_header)
 print("Spark Rooms you belong to: ")	
 print(resp.text)
